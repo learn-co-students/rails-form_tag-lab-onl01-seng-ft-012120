39ce7c17e50128c4f9c5629f60485140a2320d34
nisticism <nistacular@gmail.com>
2020-04-15 19:50:41 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class StudentsController < ApplicationController
   
   def index
@@ -7,5 +9,17 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    binding.pry
+    @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    @student.save
+    redirect_to students_path
+  end
+
+
 
 end
